d8eff809c60f5b24f339ab870d6291e9c8fdcdff
고명찬 <kmcer@naver.com>
2026-03-25 20:56:39 +0900

    add Dockerfile for render

--- src/main/java/com/example/webTest/Controller/testController.java
+++ src/main/java/com/example/webTest/Controller/testController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class testController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String test(){
         return "test";
     }
